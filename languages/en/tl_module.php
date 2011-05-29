796ddf4fc8bc1202bba54f607b8b04292850ab82
Stephan Jahrling <info@jahrling-software.de>
2011-05-29 11:12:43 +0200

    english language files

---
+++ languages/en/tl_module.php
@@ -0,0 +1,39 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Stephan Jahrling (Stephan Jahrling - Softwarelösungen), Oliver Richter (Grünfisch Webdesign) 2011
+ * @author     Stephan Jahrling <info@jahrling-software.de>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['sjstyleswitcher_fontsizes']      = array('Fontsizes', 'Fontsizes the user can switch to');
+$GLOBALS['TL_LANG']['tl_module']['sjstyleswitcher_styles'] 	       = array('Stylesheets', 'Styles the user can switch to.');
+$GLOBALS['TL_LANG']['tl_module']['sjstyleswitcher_cookielifetime'] = array('Cookie expires in (in days)', 'cookie expire time in days. Use 0 to save session cookies.');
+$GLOBALS['TL_LANG']['tl_module']['sjstyleswitcher_alternatestyles']= array('use "alternate stylesheet"', 'ädt alle Styles (Stil-Menüpunkte) als link-Tags mit der Auszeichnung rel="alternate style" in den HEAD-Bereich der Webseite.');
+
+
+?>
